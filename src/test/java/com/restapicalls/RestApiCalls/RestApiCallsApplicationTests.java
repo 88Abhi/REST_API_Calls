f6b8ffdfb61c8e446cf1ec82d7fd5327fb371bb7
Abhishek jat <abhishekjat80094@gmail.com>
2025-02-21 21:51:43 +0530

    Initial Commit

---
+++ src/test/java/com/restapicalls/RestApiCalls/RestApiCallsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapicalls.RestApiCalls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiCallsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
